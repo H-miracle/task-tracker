d49a186fc87b0da06b8183c464c0ac271d45905c
H-miracle <deboimiracle@gmail.com>
2022-06-29 12:18:11 +0000

    props added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types';
+import Button from './Button'
 
 const Header = ({title}) => {
+   const onClick = () => {
+      console.log('click')
+   }
    return (
-      <header>
+      <header className='header'>
          <h1>{title}</h1>
+         <Button color='green' text='Add' onClick={onClick} />
       </header>
    )
 }
@@ -13,7 +18,7 @@ Header.defaultProps = {
 }
  
 Header.propTypes = {
-   title: PropTypes.string,
+   title: PropTypes.string.isRequired,
 }
 
 //css in js 
